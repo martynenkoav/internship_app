afa88a7a12d5ade51243d5e4b1685b3d25f22693
Nastya <martynenkoav.18@mail.ru>
2022-05-17 22:42:38 +0300

    added all files for frontend from the previous project

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from "../model/user";
+import {FormControl, FormGroup} from "@angular/forms";
+import {Const} from "../const/const";
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) {
+  }
+
+  AUTH_URL: string = Const.LOCALHOST_URL + 'api/auth';
+
+  login(user: User): Observable<any> {
+    return this.http.post(this.AUTH_URL + '/signin', user, httpOptions);
+  }
+
+  register(user: FormGroup): Observable<any> {
+    return this.http.post(this.AUTH_URL, user, httpOptions);
+  }
+}
